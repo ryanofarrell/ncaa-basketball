61f040f09b161f619e66cb5f43471b8d439a51a3
Ryan O'Farrell <rofarrell.92@gmail.com>
2018-12-18 22:43:40 -0500

    Committing createreplacecsv UDF

--- UDFs.py
+++ UDFs.py
@@ -18,3 +18,48 @@ def printtime(prefix,timetoprint):
         print(prefix + str(round((timetoprint)/60,3)) + ' min')
 ###############################################################################
 ###############################################################################
+
+
+###############################################################################
+# Create "Replace CSV" UDF
+# Inputs: csvtoreplace (the file to replace); replacementdataframe (numeric time to print)
+# Outputs: replaces the file at the location
+###############################################################################
+def createreplacecsv(csvtoreplace,dataframetowrite):
+    # Assert cavtoreplace is a string and ends in csv
+    assert type(csvtoreplace) == str, 'First argument is not a string'
+    assert csvtoreplace[-4:] == '.csv', 'First argument is not a csv file'
+    
+    # Create new file string
+    import re
+    regex = '^(\/(?:.+\/)*)(.+)(.csv)'
+    currcsvpath = re.match(regex,csvtoreplace).group(1)
+    currcsvname = re.match(regex,csvtoreplace).group(2)
+    
+    from datetime import datetime
+    now = str(datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+    newcsvpath = currcsvpath + 'archive/'
+    newcsvname = currcsvname + '_ReplacedOn_' + now + '.csv'
+    
+    # Assert the archive path exists
+    import os
+    assert os.path.exists(newcsvpath), "No 'archive' folder in the provided path"
+    
+    # Assert dataframetowrite is a dataframe
+    import pandas as pd
+    assert type(dataframetowrite) == pd.core.frame.DataFrame, 'Second argument is not a DataFrame'
+
+
+    print('Replacing:' + csvtoreplace)
+
+    # Move current file to archive folder, rename file
+    try:
+        os.rename(csvtoreplace, newcsvpath + newcsvname)
+        print('Archived current' + currcsvname + '.csv into archive/ folder as ' + newcsvname)
+    except FileNotFoundError:
+        print('No file archived; no file named ' + currcsvname + '.csv at given path')
+    
+    dataframetowrite.to_csv(currcsvpath + currcsvname + '.csv', index=False)
+    print('Created ' + currcsvname + '.csv in ' + currcsvpath)
+###############################################################################
+###############################################################################
