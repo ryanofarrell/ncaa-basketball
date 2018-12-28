a606398ea0698afc805c5b6c904b3a94849ddde3
Ryan O'Farrell <rofarrell.92@gmail.com>
2018-12-27 21:02:31 -0500

    Creation of seasontable file, first commit, basic functionality

---
+++ CreateSeasonTableCSV.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 21 11:03:20 2018
+
+@author: Ryan
+"""
+
+
+import pandas as pd
+from UDFs import createreplacecsv
+
+
+seasonteams = pd.read_csv('/Users/Ryan/Google Drive/ncaa-basketball-data/seasonteams.csv')
+
+inclcolumns = ['Season'
+               ,'TmName'
+               ,'TmGame'
+               ,'TmWin'
+               ,'TmLoss'
+               ,'TmSoS'
+               ,'Rank_TmSoS'
+               ,'TourneyWin'
+               ,'TourneyGame'
+               ,'PlayInWin'
+               ,'PlayInGame'
+               ,'TourneySeed'
+               ,'TourneyResultStr'
+               ]
+
+
+metrics = ['Margin'
+           ,'PF'
+           ]
+
+suffixes = [
+            'perGame'
+            ,'per40'
+            ]
+
+inclmetrics = []
+
+for prefix in ['Tm','Opp']:
+    for metric in metrics:
+        for suffix in suffixes:
+            inclmetrics.append(prefix + metric + suffix)
+            inclmetrics.append('OA_' + prefix + metric + suffix)
+            inclmetrics.append('Rank_' + prefix + metric + suffix)
+            inclmetrics.append('Rank_OA_' + prefix + metric + suffix)
+
+for metric in inclmetrics:
+    inclcolumns.append(metric)
+    
+del metric, prefix, suffix
+
+# Initial trim of crazy columns
+seasontable = seasonteams[inclcolumns]
+
+
+
+testst = seasontable.loc[seasontable['TmName'] == 'Florida']
+testst = seasontable.loc[seasontable['Season'] == 2019]
+
+createreplacecsv('/Users/Ryan/Google Drive/ncaa-basketball-data/seasontable.csv',seasontable)
