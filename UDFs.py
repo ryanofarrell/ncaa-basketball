38a6e01c62a8df0a79c5e4d7ac01792257f70749
Ryan O'Farrell <rofarrell.92@gmail.com>
2019-01-17 18:14:07 -0500

    Created printtitle, dfcoldiffs, and timer class

--- UDFs.py
+++ UDFs.py
@@ -50,16 +50,82 @@ def createreplacecsv(csvtoreplace,dataframetowrite):
     assert type(dataframetowrite) == pd.core.frame.DataFrame, 'Second argument is not a DataFrame'
 
 
-    print('Writing: ' + csvtoreplace)
+    print('\nWriting: ' + currcsvname + '.csv...')
 
     # Move current file to archive folder, rename file
     try:
         os.rename(csvtoreplace, newcsvpath + newcsvname)
-        print('Archived current ' + currcsvname + '.csv into archive/ folder as ' + newcsvname)
+        print('Archived current ' + currcsvname + '.csv into archive folder...')
     except FileNotFoundError:
         print('No file archived; no file named ' + currcsvname + '.csv at given path')
     
     dataframetowrite.to_csv(currcsvpath + currcsvname + '.csv', index=False)
-    print('Wrote dataframe to ' + currcsvname + '.csv in ' + currcsvpath)
+    print('Success! Wrote dataframe to ' + currcsvname + '.csv')
+###############################################################################
+###############################################################################
+
+
+###############################################################################
+# "Title" UDF
+# Inputs: String to title
+# Outputs: printed title
+###############################################################################
+def printtitle(stringtoprint):
+    assert type(stringtoprint) == str, 'Please input a string'
+    print('\n##################################')
+    print('# ' + stringtoprint)
+    print('##################################\n')
+###############################################################################
+###############################################################################
+
+###############################################################################
+# "dfcoldiffs" UDF
+# Inputs: two dataframes, specifier of list or count
+# Outputs: the column differences
+###############################################################################
+def dfcoldiffs(df1, df2, spec = 'list'):
+    import pandas as pd
+    assert type(df1) == pd.core.frame.DataFrame, 'First input is not a DataFrame'
+    assert type(df2) == pd.core.frame.DataFrame, 'Second input is not a DataFrame'
+    assert type(spec) == str, 'Third argument is not a string'
+    assert spec in ['count','list'], 'Must request either a count or a list'
+    
+    df1_cols = sorted(list(df1))
+    df2_cols = sorted(list(df2))
+    
+    coldiffs = []
+    
+    coldiffs = [['In first, not in second',sorted(list(set(df1_cols) - set(df2_cols)))]]
+    coldiffs.append(['In second, not in first',sorted(list(set(df2_cols) - set(df1_cols)))])
+    
+    if spec == 'list':
+        return coldiffs
+    elif spec == 'count':
+        return len(coldiffs[0][1]) + len(coldiffs[1][1])
+
+###############################################################################
+###############################################################################
+
+###############################################################################
+# "timer" class
+###############################################################################
+from datetime import datetime
+class timer:
+    def __init__(self):
+        pass
+    
+    def start(self):
+        self.starttime = datetime.now()
+        self.prevsplit = datetime.now()
+        print('Timer started...')
+        
+    def split(self,descr = 'Time since prev split: '):
+        assert type(descr) == str, 'Please pass a string'
+        self.now = datetime.now()
+        print(descr +  str(self.now - self.prevsplit))
+        self.prevsplit = datetime.now()
+    def end(self):
+        self.now = datetime.now()
+        print('Time since start: ' +  str(self.now - self.starttime))
 ###############################################################################
 ###############################################################################
