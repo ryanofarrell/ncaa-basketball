fa3408e136ac1033b1d111579f675a435d72442e
Ryan O'Farrell <rofarrell.92@gmail.com>
2019-01-22 21:27:47 -0500

    Pep-8

--- CreateSeasonTableCSV.py
+++ CreateSeasonTableCSV.py
@@ -10,37 +10,34 @@ Created on Fri Dec 21 11:03:20 2018
 import pandas as pd
 from UDFs import createreplacexlsx
 
-seasonteams = pd.read_csv('/Users/Ryan/Google Drive/ncaa-basketball-data/seasonteams.csv')
+seasonteams = pd.read_csv(
+        '/Users/Ryan/Google Drive/ncaa-basketball-data/seasonteams.csv')
 
-inclcolumns = ['Season'
-               ,'TmName'
-               ,'TmGame'
-               ,'TmWin'
-               ,'TmLoss'
-               ,'TmSoS'
-               ,'Rank_TmSoS'
-               ,'TourneyWin'
-               ,'TourneyGame'
-               ,'PlayInWin'
-               ,'PlayInGame'
-               ,'TourneySeed'
-               ,'TourneyResultStr'
-               ]
+inclcolumns = ['Season',
+               'TmName',
+               'TmGame',
+               'TmWin',
+               'TmLoss',
+               'TmSoS',
+               'Rank_TmSoS',
+               'TourneyWin',
+               'TourneyGame',
+               'PlayInWin',
+               'PlayInGame',
+               'TourneySeed',
+               'TourneyResultStr']
 
 
-metrics = ['Margin'
-           ,'PF'
-           ]
+metrics = ['Margin',
+           'PF']
 
-suffixes = [
-            'perGame'
-            ,'per40'
-            ,'perPoss'
-            ]
+suffixes = ['perGame',
+            'per40',
+            'perPoss']
 
 inclmetrics = []
 
-for prefix in ['Tm','Opp']:
+for prefix in ['Tm', 'Opp']:
     for metric in metrics:
         for suffix in suffixes:
             inclmetrics.append(prefix + metric + suffix)
@@ -50,21 +47,21 @@ for prefix in ['Tm','Opp']:
 
 for metric in inclmetrics:
     inclcolumns.append(metric)
-    
+
 del metric, prefix, suffix
 
 # Initial trim of crazy columns
 seasontable = seasonteams[inclcolumns]
 
-print(seasontable.dtypes)
 for column in inclcolumns:
     if column[:4] == 'Rank':
         seasontable[column].astype(int)
 #        seasontable[metric].to_numeric(errors = 'coerce')
 
 
-
 testst = seasontable.loc[seasontable['TmName'] == 'Florida']
 testst = seasontable.loc[seasontable['Season'] == 2019]
 
-createreplacexlsx('/Users/Ryan/Google Drive/ncaa-basketball-data/seasontable.xlsx',seasontable)
+createreplacexlsx(
+        '/Users/Ryan/Google Drive/ncaa-basketball-data/seasontable.xlsx',
+        seasontable)
