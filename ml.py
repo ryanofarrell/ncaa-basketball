4b3f2d7b4a013b7cba3743050bdeb4b28842559b
Ryan O'Farrell <rofarrell.92@gmail.com>
2022-03-23 22:00:20 -0400

    Build out more ml

--- ml.py
+++ ml.py
@@ -1,13 +1,13 @@
 # %% Imports
 import numpy as np
 import pandas as pd
-from helpers import readSql
+from helpers import dfToTable, executeSql, get_unique_permutations, readSql
 from opponentAdjust import OUTPUT_METRIC_DICT
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from scipy.cluster.hierarchy import dendrogram, linkage
-from matplotlib import pyplot as plt
 import plotly.express as px
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+
 
 # %% Prep SQL for team vs opp
 sql_cols = [
@@ -47,70 +47,116 @@ raw_td = readSql(q).dropna(how="any")
 # %%
 td = raw_td.copy()
 X_cols = td.columns[8:]
+
+td["tm_margin"] = td["tm_pts"] - td["opp_pts"]
+
+# De-duplicate - apply random number to column, keep first
 td["rand"] = np.random.random(len(td))
 td = (
     td.sort_values(by=["rand"])
     .drop_duplicates(subset=["date", "game_key"], keep="first")
     .drop(["rand"], axis=1)
 )
-# De-duplicate - apply random number to column, keep first
-X = td[X_cols]
-y = td["tm_win"]
+assert len(td) == 0.5 * len(raw_td), "Dropped != half games"
 
-#
+X = td[X_cols]
+assert len(X.columns) == 4 * len(OUTPUT_METRIC_DICT), "INCORRECT X COLS"
+
 # Scale
 x_scale = StandardScaler().fit_transform(X)
 
 # pca
 pca = PCA(n_components=0.9, random_state=832828).fit(x_scale)
-
 i = 1
 cuml = 0
 for ev in pca.explained_variance_ratio_:
     cuml += ev
     print(f"Expl Var % by {i}: {ev:.3%} (cuml: {cuml:.3%})")
     i += 1
-
-
 x_scale_pca = pca.transform(x_scale)
+print(f"Dropped num variables by {1 - x_scale_pca.shape[1] / X.shape[1]:.2%} via PCA")
 
-# %% hierarchical
-from sklearn.ensemble import RandomForestClassifier
+# %% Loop through years and train
+all_years = sorted(td["season"].unique().tolist())
 
-rfc = RandomForestClassifier(
-    n_estimators=5000,
-    min_samples_split=5,
-    max_features="sqrt",
-    bootstrap=True,
-    n_jobs=-1,
-    max_samples=0.8,
-    verbose=True,
-    random_state=918375,
-)
+if input("Drop/remake old table? y/(n): ") == "y":
+    print(f"Dropping old table")
+    executeSql("drop table if exists predictions")
+    q = f"""
+    create table predictions (
+        config string not null,
+        season integer not null,
+        date string not null,
+        game_key string not null,
+        tm_teamid integer not null,
+        opp_teamid integer not null,
+        tm_pts integer not null,
+        opp_pts integer not null,
+        tm_margin integer not null,
+        tm_win integer not null,
+        pred_win integer not null,
+        pred_win_prob real not null,
+        pred_margin real not null,
+        primary key (config, date asc, game_key asc)
+    )
+    """
+    executeSql(q)
+    for p in get_unique_permutations(["config", "date", "game_key"]):
+        executeSql(
+            f"CREATE INDEX predictions_{'_'.join(p)} on predictions ({', '.join(p)})"
+        )
 
-# Train-est split
-train_mask = td["season"] != 2019
-test_mask = td["season"] == 2019
-X_train = x_scale_pca[train_mask]
-X_test = x_scale_pca[test_mask]
-y_train = y[train_mask]
-y_test = y[test_mask]
 
-rfc.fit(X_train, y_train)
-# %%
+config = {
+    "model_type": "regression",
+    "y_val": "tm_margin",
+    "n_samples": 15000,
+    "min_split": 5,
+    "max_feat": "sqrt",
+    "max_samp": 0.8,
+}
+y = td[config["y_val"]]
+for test_year in all_years:
+    print(f"Testing {test_year} season")
+    if config["model_type"] == "regression":
+        rf_class = RandomForestRegressor
+    elif config["model_type"] == "classification":
+        rf_class = RandomForestClassifier
+    else:
+        raise ValueError(f"Unconfigd model_type: {config['model_type']}")
 
-td_test = td.loc[test_mask]
-td_test = td_test[[x for x in td_test if x not in X_cols]]
-td_test["margin"] = td_test["tm_pts"] - td_test["opp_pts"]
-td_test["abs_margin"] = np.abs(td_test["margin"])
-td_test["actl"] = y_test
-td_test["pred"] = rfc.predict(X_test)
-td_test["correct"] = 1 * (td_test["actl"] == td_test["pred"])
-td_test["pred_prob"] = rfc.predict_proba(X_test)[:, 1]
-td_test["certainty"] = np.abs(0.5 - td_test["pred_prob"]) * 2
+    rf = rf_class(
+        n_estimators=config["n_samples"],
+        min_samples_split=config["min_split"],
+        max_features=config["max_feat"],
+        bootstrap=True,
+        n_jobs=-1,
+        max_samples=config["max_samp"],
+        verbose=True,
+        random_state=918375,
+    )
 
-# %%
+    # Train-est split
+    train_mask = td["season"] != test_year
+    test_mask = td["season"] == test_year
+    X_train = x_scale_pca[train_mask]
+    X_test = x_scale_pca[test_mask]
+    y_train = y[train_mask]
+    y_test = y[test_mask]
 
-px.histogram(td_test, x="certainty", y="correct", histfunc="avg", nbins=30)
+    rf.fit(X_train, y_train)
 
-# %%
+    # Save output
+    td_test = td.loc[test_mask][[x for x in td.columns if x not in X_cols]]
+    td_test = td_test[[x for x in td_test if x not in X_cols]]
+    td_test["config"] = str(list(config.items()))[1:-1]
+    if config["model_type"] == "classification":
+        td_test["pred_win"] = rf.predict(X_test)
+        td_test["pred_margin"] = 0
+        td_test["pred_win_prob"] = rf.predict_proba(X_test)[:, 1]
+    else:  # Classification
+        td_test["pred_margin"] = rf.predict(X_test)
+        td_test["pred_win_prob"] = 0
+        td_test["pred_win"] = 1 * (td_test["pred_margin"] > 0)
+
+    dfToTable(td_test, "predictions", "ncaa.db", "append")
