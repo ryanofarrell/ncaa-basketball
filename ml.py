1d4c90cfcfa663ff6dcc178cd19234d42e1c3250
Ryan O'Farrell <rofarrell.92@gmail.com>
2022-06-01 20:10:58 -0400

    Loop through many models and save all outputs

--- ml.py
+++ ml.py
@@ -7,12 +7,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import plotly.express as px
 from sklearn.ensemble import (
+    GradientBoostingClassifier,
     RandomForestClassifier,
     RandomForestRegressor,
     GradientBoostingRegressor,
 )
 
-
+RANDOMSEED = 837592
 # %% Prep SQL for team vs opp
 def get_raw_data():
     sql_cols = [
@@ -33,6 +34,8 @@ def get_raw_data():
         ,g.tm_pts
         ,g.opp_pts
         ,g.tm_win
+        ,v.tm_closeline
+        ,v.tm_ml
         ,td1.{', td1.'.join(tm1_sql_cols)}
         ,td2.{', td2.'.join(tm2_sql_cols)}
     from games g
@@ -44,11 +47,21 @@ def get_raw_data():
         and g.opp_teamid = td2.teamid
     left join calendar c
         on c.date = g.date
+    left join vegas_lines v
+        on g.season = v.season
+        and g.date = v.date
+        and g.game_key = v.game_key
+        and g.tm_teamid = v.tm_teamid
     where c.day_num >= 60
-    order by g.date, game_key, tm_teamid
+    order by g.date, g.game_key, g.tm_teamid
     -- limit 1000
     """
-    raw_td = readSql(q).dropna(how="any")
+    raw_td = readSql(q)
+    raw_td.dropna(
+        how="any",
+        subset=[col for col in raw_td.columns if col[:3] in ["tm1", "tm2"]],
+        inplace=True,
+    )
     return raw_td
 
 
@@ -62,24 +75,30 @@ def prompt_remake_predictions_table():
     executeSql("drop table if exists predictions")
     q = f"""
     create table predictions (
+        id string not null,
         config string not null,
         season integer not null,
         date string not null,
         game_key string not null,
         tm_teamid integer not null,
         opp_teamid integer not null,
+        tm_closeline real not null,
+        tm_ml real not null,
         tm_pts integer not null,
         opp_pts integer not null,
         tm_margin integer not null,
+        tm_margin_ats integer not null,
         tm_win integer not null,
-        pred_win integer not null,
+        tm_win_ats integer not null,
         pred_win_prob real not null,
+        pred_win_ats_prob real not null,
         pred_margin real not null,
-        primary key (config, date asc, game_key asc)
+        pred_margin_ats real not null,
+        primary key (id, config, date asc, game_key asc, tm_teamid asc)
     )
     """
     executeSql(q)
-    for p in get_unique_permutations(["config", "date", "game_key"]):
+    for p in get_unique_permutations(["id", "config", "date", "game_key", "tm_teamid"]):
         executeSql(
             f"CREATE INDEX predictions_{'_'.join(p)} on predictions ({', '.join(p)})"
         )
@@ -91,110 +110,266 @@ if __name__ == "__main__":
     td = get_raw_data()
 
     # Prep some columns
-    X_cols = td.columns[8:]
+    X_cols = [col for col in td.columns if col[:3] in ["tm1", "tm2"]]
     td["tm_margin"] = td["tm_pts"] - td["opp_pts"]
-
-    # De-duplicate - apply random number to column, keep first
-    td["rand"] = np.random.random(len(td))
-    preLen = len(td)
-    td = (
-        td.sort_values(by=["rand"])
-        .drop_duplicates(subset=["date", "game_key"], keep="first")
-        .drop(["rand"], axis=1)
+    td["tm_margin_ats"] = td["tm_pts"] - td["opp_pts"] + td["tm_closeline"]
+    td["tm_win_ats"] = td["tm_margin_ats"] > 0
+    # Only include data with a line to evertually evaluate the outcomes
+    td["tm_ml"] = td["tm_ml"].astype(int, errors="ignore")
+    td = td.loc[~(pd.isna(td["tm_closeline"]) | pd.isna(td["tm_ml"]))].reset_index(
+        drop=True
     )
-    assert len(td) == 0.5 * preLen, "Dropped != half games"
 
+    # Set up X matrices
     X = td[X_cols]
+    X_INCL_LINE = td[X_cols + ["tm_closeline"]]
     assert len(X.columns) == 4 * len(OUTPUT_METRIC_DICT), "INCORRECT X COLS"
 
     # Scale
     x_scale = StandardScaler().fit_transform(X)
+    x_scale_incl_line = StandardScaler().fit_transform(X_INCL_LINE)
 
     # pca
-    pca = PCA(n_components=0.9, random_state=832828).fit(x_scale)
-    i = 1
-    cuml = 0
-    for ev in pca.explained_variance_ratio_:
-        cuml += ev
-        print(f"Expl Var % by {i}: {ev:.3%} (cuml: {cuml:.3%})")
-        i += 1
-    x_scale_pca = pca.transform(x_scale)
+    x_scale_pca = PCA(n_components=0.99, random_state=832828).fit_transform(x_scale)
     print(
         f"Dropped num variables by {1 - x_scale_pca.shape[1] / X.shape[1]:.2%} via PCA"
     )
+    x_scale_pca_incl_line = PCA(n_components=0.99, random_state=832828).fit_transform(
+        x_scale_incl_line
+    )
+    print(
+        f"Dropped num variables by {1 - x_scale_pca.shape[1] / X_INCL_LINE.shape[1]:.2%} via PCA"
+    )
+
+    # Set up models
+    reg_models = {
+        "rfr_10000": RandomForestRegressor(
+            n_estimators=10000,
+            min_samples_split=5,
+            max_features="sqrt",
+            max_samples=0.8,
+            random_state=RANDOMSEED,
+            verbose=True,
+            n_jobs=-1,
+        ),
+        "rfr_5000": RandomForestRegressor(
+            n_estimators=5000,
+            min_samples_split=5,
+            max_features="sqrt",
+            max_samples=0.8,
+            random_state=RANDOMSEED,
+            verbose=True,
+            n_jobs=-1,
+        ),
+        "rfr_1000_verysubsampled": RandomForestRegressor(
+            n_estimators=1000,
+            min_samples_split=5,
+            max_features="sqrt",
+            max_samples=0.2,
+            random_state=RANDOMSEED,
+            verbose=True,
+            n_jobs=-1,
+        ),
+        "rfr_1000_shallow": RandomForestRegressor(
+            n_estimators=1000,
+            max_depth=5,
+            max_features="sqrt",
+            max_samples=0.8,
+            random_state=RANDOMSEED,
+            verbose=True,
+            n_jobs=-1,
+        ),
+        "gbr_5000": GradientBoostingRegressor(
+            n_estimators=5000,
+            learning_rate=0.1,
+            subsample=0.8,
+            min_samples_split=5,
+            max_features="sqrt",
+            random_state=RANDOMSEED,
+            verbose=True,
+        ),
+        "gbr_2500": GradientBoostingRegressor(
+            n_estimators=2500,
+            learning_rate=0.1,
+            subsample=0.8,
+            min_samples_split=5,
+            max_features="sqrt",
+            random_state=RANDOMSEED,
+            verbose=True,
+        ),
+        "gbr_1000_slowlearn": GradientBoostingRegressor(
+            n_estimators=1000,
+            learning_rate=0.01,
+            subsample=0.8,
+            min_samples_split=5,
+            max_features="sqrt",
+            random_state=RANDOMSEED,
+            verbose=True,
+        ),
+        "gbr_1000_shallow": GradientBoostingRegressor(
+            n_estimators=1000,
+            learning_rate=0.1,
+            subsample=0.8,
+            max_depth=5,
+            max_features="sqrt",
+            random_state=RANDOMSEED,
+            verbose=True,
+        ),
+    }
+    # Set up models
+    class_models = {
+        "rfc_10000": RandomForestClassifier(
+            n_estimators=10000,
+            min_samples_split=5,
+            max_features="sqrt",
+            max_samples=0.8,
+            random_state=RANDOMSEED,
+            verbose=True,
+            n_jobs=-1,
+        ),
+        "rfc_5000": RandomForestClassifier(
+            n_estimators=5000,
+            min_samples_split=5,
+            max_features="sqrt",
+            max_samples=0.8,
+            random_state=RANDOMSEED,
+            verbose=True,
+            n_jobs=-1,
+        ),
+        "rfc_1000_verysubsampled": RandomForestClassifier(
+            n_estimators=1000,
+            min_samples_split=5,
+            max_features="sqrt",
+            max_samples=0.2,
+            random_state=RANDOMSEED,
+            verbose=True,
+            n_jobs=-1,
+        ),
+        "rfc_1000_shallow": RandomForestClassifier(
+            n_estimators=1000,
+            max_depth=5,
+            max_features="sqrt",
+            max_samples=0.8,
+            random_state=RANDOMSEED,
+            verbose=True,
+            n_jobs=-1,
+        ),
+        "gbc_5000": GradientBoostingClassifier(
+            n_estimators=5000,
+            learning_rate=0.1,
+            subsample=0.8,
+            min_samples_split=5,
+            max_features="sqrt",
+            random_state=RANDOMSEED,
+            verbose=True,
+        ),
+        "gbc_2500": GradientBoostingClassifier(
+            n_estimators=2500,
+            learning_rate=0.1,
+            subsample=0.8,
+            min_samples_split=5,
+            max_features="sqrt",
+            random_state=RANDOMSEED,
+            verbose=True,
+        ),
+        "gbc_1000_slowlearn": GradientBoostingClassifier(
+            n_estimators=1000,
+            learning_rate=0.01,
+            subsample=0.8,
+            min_samples_split=5,
+            max_features="sqrt",
+            random_state=RANDOMSEED,
+            verbose=True,
+        ),
+        "gbc_1000_shallow": GradientBoostingClassifier(
+            n_estimators=1000,
+            learning_rate=0.1,
+            subsample=0.8,
+            max_depth=5,
+            max_features="sqrt",
+            random_state=RANDOMSEED,
+            verbose=True,
+        ),
+    }
 
     # Remake predictions table if yes
     prompt_remake_predictions_table()
 
-    # %% Loop through years and train
-
-    config = {
-        "model_class": "gb",
-        "model_type": "regression",
-        "y_val": "tm_margin",
-        "n_estimators": 5000,
-        "min_split": 5,
-        "max_feat": "sqrt",
-        "max_samp": 0.8,
-    }
-
-    match config["model_class"]:
-        case "rf":
-            match config["model_type"]:
-                case "regression":
-                    rf_class = RandomForestRegressor
-                case "classification":
-                    rf_class = RandomForestClassifier
-                case _:
-                    raise ValueError("Unconfig")
-            model = rf_class(
-                n_estimators=config["n_estimators"],
-                min_samples_split=config["min_split"],
-                max_features=config["max_feat"],
-                bootstrap=True,
-                n_jobs=-1,
-                max_samples=config["max_samp"],
-                verbose=True,
-                random_state=918375,
-            )
-        case "gb":
-            model = GradientBoostingRegressor(
-                n_estimators=config["n_estimators"],
-                subsample=config["max_samp"],
-                min_samples_split=config["min_split"],
-                max_features=config["max_feat"],
-                random_state=918375,
-                verbose=True,
-            )
-        case _:
-            raise ValueError("unconfigd")
-
-    y = td[config["y_val"]]
     all_years = sorted(td["season"].unique().tolist())
     for test_year in all_years:
         print(f"Testing {test_year} season")
 
-        # Train-est split
+        # Train-test split
         train_mask = td["season"] != test_year
         test_mask = td["season"] == test_year
         X_train = x_scale_pca[train_mask]
         X_test = x_scale_pca[test_mask]
-        y_train = y[train_mask]
-        y_test = y[test_mask]
+        X_train_incl_line = x_scale_pca_incl_line[train_mask]
+        X_test_incl_line = x_scale_pca_incl_line[test_mask]
 
-        model.fit(X_train, y_train)
+        for id, model in reg_models.items():
+            already_done = (
+                readSql("Select distinct season, id from predictions")
+                .to_numpy()
+                .tolist()
+            )
+            if [test_year, id] in already_done:
+                print(f"Skipping {id} for {test_year}")
+                continue
+            # Set up output
+            td_test = td.loc[test_mask][[x for x in td.columns if x not in X_cols]]
+            td_test["id"] = id
+            td_test["config"] = str(model.get_params())
 
-        # Save output
-        td_test = td.loc[test_mask][[x for x in td.columns if x not in X_cols]]
-        td_test = td_test[[x for x in td_test if x not in X_cols]]
-        td_test["config"] = str(list(config.items()))[1:-1]
-        if config["model_type"] == "classification":
-            td_test["pred_win"] = model.predict(X_test)
-            td_test["pred_margin"] = 0
-            td_test["pred_win_prob"] = model.predict_proba(X_test)[:, 1]
-        else:  # Classification
+            # Get margin prediction
+            y = td["tm_margin"]
+            y_train = y[train_mask]
+            y_test = y[test_mask]
+            model.fit(X_train, y_train)
             td_test["pred_margin"] = model.predict(X_test)
-            td_test["pred_win_prob"] = 0
-            td_test["pred_win"] = 1 * (td_test["pred_margin"] > 0)
 
-        dfToTable(td_test, "predictions", "ncaa.db", "append")
+            # Get ATS margin
+            y = td["tm_margin_ats"]
+            y_train = y[train_mask]
+            y_test = y[test_mask]
+            model.fit(X_train_incl_line, y_train)
+            td_test["pred_margin_ats"] = model.predict(X_test_incl_line)
+
+            td_test["pred_win_prob"] = 0
+            td_test["pred_win_ats_prob"] = 0
+
+            dfToTable(td_test, "predictions", "ncaa.db", "append")
+
+        for id, model in class_models.items():
+            already_done = (
+                readSql("Select distinct season, id from predictions")
+                .to_numpy()
+                .tolist()
+            )
+            if [test_year, id] in already_done:
+                print(f"Skipping {id} for {test_year}")
+                continue
+
+            # Set up output
+            td_test = td.loc[test_mask][[x for x in td.columns if x not in X_cols]]
+            td_test["id"] = id
+            td_test["config"] = str(model.get_params())
+
+            # Get win prediction
+            y = td["tm_win"]
+            y_train = y[train_mask]
+            y_test = y[test_mask]
+            model.fit(X_train, y_train)
+            td_test["pred_win_prob"] = model.predict_proba(X_test)[:, 1]
+
+            # Get ATS win margin
+            y = td["tm_win_ats"]
+            y_train = y[train_mask]
+            y_test = y[test_mask]
+            model.fit(X_train_incl_line, y_train)
+            td_test["pred_win_ats_prob"] = model.predict_proba(X_test_incl_line)[:, 1]
+
+            td_test["pred_margin"] = 0
+            td_test["pred_margin_ats"] = 0
+
+            dfToTable(td_test, "predictions", "ncaa.db", "append")
